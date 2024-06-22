1c62828a4dd7098e2a0f17d2240b74b5b2d98096
makaloni6 <work.honeycomb@gmail.com>
2024-06-22 14:55:42 +0900

    機能追加

--- add_quantity.py
+++ add_quantity.py
@@ -1,4 +1,5 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont  # type: ignore
+import csv
 import os
 import re
 import pickle
@@ -25,28 +26,36 @@ def getImages() -> list:
 def getContent() -> dict:
     with open('./pickle/contents.pickle', 'rb') as f:
         contents = pickle.load(f)
+
+    with open('title_fixed.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = [row for row in reader]
     
+    for d in data:
+        contents[d[0]] = [d[1], d[2]]
+
     return contents
 
 
-def getCodes() -> dict:
-    with open('./pickle/code_dict.pickle', 'rb') as f:
+def getCodes(acc: str) -> dict:
+    with open('./pickle/code_dict_{}.pickle'.format(acc), 'rb') as f:
         code_dict = pickle.load(f)
     return code_dict
 
 
 def main():
+    acc = input()
     text_template, font = textSetter()
     imgs = getImages()
     contents = getContent()
-    code_dict = getCodes()
+    code_dict = getCodes(acc)
     errorImgs = []
 
     for img_name in imgs:
 
         img = Image.open('./imgs/{}'.format(img_name))
         id = img_name[:-4]
-        
+
         xy = list(img.size)
         if xy[0] > 2000:
             font = ImageFont.truetype('ヒラギノ角ゴシック W6.ttc', index=3, size=300)
@@ -62,22 +71,25 @@ def main():
         if 'リットル' in content:
             content = re.findall(r'[0-9.]+', content)[0] + 'L'
         text = text_template.format(content, q)
-       
+
         bbox = draw.multiline_textbbox((0, 0), text, font=font)
-        
+
         if not coverageCheck(bbox, xy):
             errorImgs.append(img)
             continue
-        
+
         # 描画
         draw.text((xy[0] - bbox[2] - 50, xy[1] - bbox[3] - 50), text, 'black', font=font)
 
         img_name = code_dict[id]
-        
+
         img.save('modifiedImgs/{}.jpg'.format(img_name))
 
     with open('error.pickle', 'wb') as f:
         pickle.dump(errorImgs, f)
     print(len(errorImgs))
+    print('finished {}'.format(acc))
+
+
 if __name__ == '__main__':
     main()
